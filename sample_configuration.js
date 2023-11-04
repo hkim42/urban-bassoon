cb0e92b15640a674fe29a6245f026529cc00b282
hkim42 <hyunsoo.david.kim@gmail.com>
2023-11-04 09:01:08 -0400

    add BU Central latitude and longitude, add function to export to main html

--- sample_configuration.js
+++ sample_configuration.js
@@ -3,3 +3,11 @@ const myApiKey = "your maps api key here";
 function getApiKey() {
     return myApiKey;
 }
+
+// Boston University Central
+const latitude = 42.3500
+const longitude = -71.1071
+
+function getLatLng() {
+    return {"lat": latitude, "lng": longitude};
+}
